e320fa76cca2e61ae1d355c950e3a401ec0417a0
Sean Liao <seankhliao@gmail.com>
2020-04-11 01:35:33 +0200

    zsh config

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
-#
+
+export PROMPT_EOL_MARK=''
+
 function _preexec() {
     typeset -g prompt_timestamp=$EPOCHSECONDS
 }
@@ -7,11 +9,13 @@ function _preexec() {
 function _precmd() {
     integer elapsed=$(( EPOCHSECONDS - ${prompt_timestamp:-$EPOCHSECONDS} ))
     local human="$(( elapsed / 3600 )):${(l:2::0:)$(( elapsed / 60 % 60 ))}:${(l:2::0:)$(( elapsed % 60 ))}"
-
-    local git=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    vcs_info
     local newline=$'\n%{\r%}'
 
     PROMPT="${newline}%F{green}%*%f %F{blue}%~%f %F{yellow}${human}%f${newline}"
-    PROMPT+="%F{242}${STY:+${STY} }${VIRTUAL_ENV:+${VIRTUAL_ENV} }${git:+${git} }%f"
+    PROMPT+="%F{242}${STY:+${STY} }${VIRTUAL_ENV:+${VIRTUAL_ENV} }${vcs_info_msg_0_:+${vcs_info_msg_0_} }%f"
     PROMPT+="%(?.%F{magenta}.%F{red})${SSH_CONNECTION+%n@%m}»%f "
 }
+
+add-zsh-hook precmd  _precmd
+add-zsh-hook preexec _preexec

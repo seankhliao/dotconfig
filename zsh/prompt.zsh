e02197589bce1f70a25123d5c27ffcfec6cfc5d9
Sean Liao <seankhliao@gmail.com>
2020-04-10 22:37:37 +0200

    update scripts

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,27 +5,13 @@ function _preexec() {
 }
 
 function _precmd() {
-    # https://github.com/sindresorhus/pretty-time-zsh
     integer elapsed=$(( EPOCHSECONDS - ${prompt_timestamp:-$EPOCHSECONDS} ))
-    local days=$(( elapsed / 60 / 60 / 24 ))
-    local hours=$(( elapsed / 60 / 60 % 24 ))
-    local minutes=$(( elapsed / 60 % 60 ))
-    local seconds=$(( elapsed % 60 ))
-    local human
-    (( days > 0 )) && human+="${days}d "
-    (( hours > 0 )) && human+="${hours}h "
-    (( minutes > 0 )) && human+="${minutes}m "
-    human+="${seconds}s"
+    local human="$(( elapsed / 3600 )):${(l:2::0:)$(( elapsed / 60 % 60 ))}:${(l:2::0:)$(( elapsed % 60 ))}"
 
-    local git_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    local git=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
     local newline=$'\n%{\r%}'
 
-    # time path exectime
-    PROMPT="${newline}%F{green}%*%f %F{blue}%~%f %F{yellow}${human}%f${newline}%F{242}"
-    [[ -n "${STY}" ]] && PROMPT+="${STY%.*} "
-    [[ -n "${VIRTUAL_ENV}" ]] && PROMPT+="${VIRTUAL_ENV:t} "
-    [[ -n "${git_branch}" ]] && PROMPT+="${git_branch} "
-    PROMPT+="%f%(?.%F{magenta}.%F{red})"
-    [[ -n "${SSH_CONNECTION}" ]] && PROMPT+="%n@%m"
-    PROMPT+="» %f"
+    PROMPT="${newline}%F{green}%*%f %F{blue}%~%f %F{yellow}${human}%f${newline}"
+    PROMPT+="%F{242}${STY:+${STY} }${VIRTUAL_ENV:+${VIRTUAL_ENV} }${git:+${git} }%f"
+    PROMPT+="%(?.%F{magenta}.%F{red})${SSH_CONNECTION+%n@%m}»%f "
 }

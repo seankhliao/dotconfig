debd609d8f6bf2b9c0a01f644bc19c4024e5d90b
Sean Liao <seankhliao@gmail.com>
2020-12-11 23:28:49 +0100

    z

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,10 +4,8 @@ bashcompinit
 complete -o nospace -C kustomize kustomize
 complete -o nospace -C terraform terraform
 
-[[ -d /opt/google-cloud-sdk/bin ]] && export PATH="${PATH}:/opt/google-cloud-sdk/bin"
 [[ -d ${XDG_DATA_HOME}/google-cloud-sdk/bin ]] && \
     export PATH="${PATH}:${XDG_DATA_HOME}/google-cloud-sdk/bin"
-[[ -f /opt/google-cloud-sdk/completion.zsh.inc ]] && source /opt/google-cloud-sdk/completion.zsh.inc
 [[ -f ${XDG_DATA_HOME}/google-cloud-sdk/completion.zsh.inc ]] && \
     source ${XDG_DATA_HOME}/google-cloud-sdk/completion.zsh.inc
 [[ -f ${ZDOTDIR}/_kubectl ]] && source ${ZDOTDIR}/_kubectl
--- zsh/function.zsh
+++ zsh/function.zsh
@@ -15,17 +15,37 @@ function gsall() {
 
     for dir in ./*; do
         if [[ -d ${dir} ]] && [[ -d ${dir}/.git ]]; then
-            if ! git -C ${dir} diff-index --quiet HEAD || ! git -C ${dir} diff-index --quiet origin/HEAD; then
-                echo "\n${bold}$dir${normal}"
-                git -C ${dir} log --oneline origin/HEAD..HEAD
+            if git -C ${dir} remote | grep origin >/dev/null ; then
+                local default=$(git -C ${dir} remote show origin | sed -n -e 's/\s*HEAD branch:\s*\([^\s]*\)/\1/p')
+                if ! git -C ${dir} diff-index --quiet --exit-code origin/${default} ; then
+                    printf "\n${bold}$dir${normal}\n"
+                    git -C ${dir} log --oneline origin/${default}..HEAD
+                    git -C ${dir} status -s
+                fi
+            elif git -C ${dir} diff-index --quiet --exit-code HEAD ; then
+                printf "\n${bold}$dir${normal}\n"
+                git -C ${dir} branch --show-current
                 git -C ${dir} status -s
             fi
         fi
     done
 }
 
+function glall() {
+    local bold=$(tput bold)
+    local normal=$(tput sgr0)
+
+    for dir in ./*; do
+        if [[ -d ${dir} ]] && [[ -d ${dir}/.git ]]; then
+            printf "\n${bold}$dir${normal}\n"
+            git -C ${dir} checkout $(git -C ${dir} symbolic-ref --short HEAD)
+            git -C ${dir} pull --prune --prune-tags --ff-only --jobs=$(nproc)
+        fi
+    done
+}
+
 function md() {
-    [[ -z ${1// } ]] && echo "no directory name gived" && return 1
+    [[ -z ${1// } ]] && echo "no directory name given" && return 1
     mkdir -p "$1" && cd "$1"
 }
 

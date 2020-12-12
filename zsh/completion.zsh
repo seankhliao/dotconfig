160a676cecfeed967b8d206183ddf3518a1edfa1
Sean Liao <seankhliao@gmail.com>
2020-12-12 01:08:48 +0100

    z

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,10 +1,10 @@
+fpath=(${ZDOTDIR} $fpath)
+
 compinit -d ${XDG_CACHE_HOME}/.zcompdump
 bashcompinit
 
 complete -o nospace -C kustomize kustomize
 complete -o nospace -C terraform terraform
-
-fpath=(${ZDOTDIR} $fpath)
 
 [[ -f ${XDG_DATA_HOME}/google-cloud-sdk/completion.zsh.inc ]] && \
     source ${XDG_DATA_HOME}/google-cloud-sdk/completion.zsh.inc

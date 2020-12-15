44ff310250a31b3d1e9f69d91183ad35f39c4458
Sean Liao <seankhliao@gmail.com>
2020-12-15 12:23:42 +0100

    cleanup

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -61,19 +61,3 @@ function {
         fi
     done < "${gitconfig}"
 }
-
-_kube_contexts()
-{
-    local curr_arg;
-    curr_arg=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( $(compgen -W "$(kubectl config get-contexts --output='name')" -- $curr_arg ) );
-}
-complete -F _kube_contexts kc
-
-_kube_namespaces()
-{
-    local curr_arg;
-    curr_arg=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( $(compgen -W "$(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')" -- $curr_arg ) );
-}
-complete -F _kube_namespaces kn

990e428625f1121e4daabcf7057bcec1a57491cf
Sean Liao <seankhliao@gmail.com>
2021-11-18 22:37:41 +0100

    more

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,7 +87,7 @@ end
 
 
 
-local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_install_path = vim.env.XDG_DATA_HOME .. '/nvim/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', packer_install_path})
     vim.cmd [[ packadd packer.nvim ]]

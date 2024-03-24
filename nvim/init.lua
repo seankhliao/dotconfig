52fd87dd78f49725786ce06c4cce7282ef141ca9
Sean Liao <sean+git@liao.dev>
2024-03-24 14:27:36 +0000

    cleanup config

--- nvim/init.lua
+++ nvim/init.lua
@@ -507,14 +507,13 @@ require("pckr").add({
             cmp.setup({
                 mapping = {
                     ["<CR>"] = cmp.mapping({
-                        -- i = function(fallback)
-                        --   if cmp.visible() and cmp.get_active_entry() then
-                        --     cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-                        --   else
-                        --     fallback()
-                        --   end
-                        -- end,
-                        i = cmp.mapping.confirm({ select = true }),
+                        i = function(fallback)
+                            if cmp.visible() and cmp.get_active_entry() then
+                                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                            else
+                                fallback()
+                             end
+                        end,
                         s = cmp.mapping.confirm({ select = true }),
                         c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
                     }),

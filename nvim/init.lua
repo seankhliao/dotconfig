92b2420da2ad096cb0e8e95204f819c8735d2429
Sean Liao <sean+git@liao.dev>
2024-10-22 19:15:34 +0100

    fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -624,26 +624,33 @@ vim.api.nvim_set_keymap("n", "ss", '"_dd', { noremap = true })
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true, silent = true })
 
 -- more format on writes
-function goimports(wait_ms)
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
     local params = vim.lsp.util.make_range_params()
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-            if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-            else
-                vim.lsp.buf.execute_command(r.command)
-            end
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
         end
+      end
     end
-end
+    vim.lsp.buf.format({async = false})
+  end
+})
 
 vim.api.nvim_exec(
     [[
 augroup Clean
 autocmd!
-autocmd BufWritePre *.go    silent :lua goimports(1000)
 autocmd BufWritePre *.go    silent :lua vim.lsp.buf.format()
 autocmd BufWritePre *       silent :FormatWrite
 augroup END
